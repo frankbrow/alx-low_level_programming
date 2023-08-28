8ac67c97e9c2f83115c25f135a3a0ec4b2a13742
frankbrow <frankamedulife@gmail.com>
2023-08-28 07:46:37 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -18,5 +18,6 @@ char *_strpbrk(char *s, char *accept)
                         }
                 s++;
                 }
+
         return ('\0');
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,6 @@
 void print_diagsums(int *a, int size)
 {
         int sum1, sum2, y;
-
         sum1 = 0;
         sum2 = 0;
 
@@ -22,6 +21,5 @@ void print_diagsums(int *a, int size)
         {
                 sum2 += a[y * size + (size - y - 1)];
         }
-
         printf("%d, %d\n", sum1, sum2);
 }
