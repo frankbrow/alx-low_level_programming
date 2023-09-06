877a005ebae95675df858c2a9cc5801546c4b3a8
frankbrow <frankamedulife@gmail.com>
2023-09-06 03:24:21 -0700

    C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,17 +11,4 @@ int main(int argc, char *argv[])
 
 printf("%s\n", argv[0]);
 return (0);
-}#include <stdio.h>
-/**
- * main - print program name
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
- */
-int main(int argc, char *argv[])
-{
-(void) argc;
-
-printf("%s\n", argv[0]);
-return (0);
 }
