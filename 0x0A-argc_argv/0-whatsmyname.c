d874c6b00de04076d15315732bc7f2de0ad7811b
frankbrow <frankamedulife@gmail.com>
2023-09-06 02:58:40 -0700

    C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
-int main(int argc _attribute_((unused)), char *argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+(void) argc;
 
-	return (0);
+printf("%s\n", argv[0]);
+return (0);
+}#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+
+printf("%s\n", argv[0]);
+return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - prints number of arguments
+ * @argc: arguments count
+ * @argv: argument vactor
+ * Return: 0
  */
-
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-(void) argv; /Ignore argv/
-printf("%d\n", argc - 1);
+(void) argv;
 
+printf("%d\n", argc - 1);
 return (0);
 }
